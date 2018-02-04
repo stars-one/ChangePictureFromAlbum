5b5a1252548f3902559cd743f76bf1fb3a560293
Stars-One <Stars-One@users.noreply.github.com>
2018-02-04 20:34:47 +0800

    增加Sharepreference保存ImageView状态

--- app/src/main/java/com/wan/changeimagetest/MainActivity.java
+++ app/src/main/java/com/wan/changeimagetest/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.ContentUris;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        load();
     }
 
     private void initView() {
@@ -153,12 +155,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //展示图片
     private void displayImage(String imagePath) {
         if (imagePath != null) {
+            save(imagePath);
             Bitmap bitImage = BitmapFactory.decodeFile(imagePath);//格式化图片
-
             mImage.setImageBitmap(bitImage);//为imageView设置图片
 
         } else {
             Toast.makeText(MainActivity.this, "获取图片失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void save(String imagePath){
+        SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();//获得SHaredPreferences.Editor对象
+        editor.putBoolean("imageChange",true);//添加一个名为imageChange的boolean值，数值为true
+        editor.putString("imagePath",imagePath);//保存imagePath图片路径
+        editor.apply();//提交
+    }
+    private void load() {
+        SharedPreferences preferences = getSharedPreferences("data", MODE_PRIVATE);//获得SharedPreferences的对象
+        //括号里的判断是去找imageChange这个对应的数值，若是找不到，则是返回false，找到了的话就是我们上面定义的true，就会执行其中的语句
+        if (preferences.getBoolean("imageChange", false)) {
+            String imagePath = preferences.getString("imagePath", "");//取出保存的imagePath，若是找不到，则是返回一个空
+            displayImage(imagePath);//调用显示图片方法，为ImageView设置图片
         }
     }
 }
